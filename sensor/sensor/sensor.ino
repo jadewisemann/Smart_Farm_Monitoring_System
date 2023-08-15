fbd6f3ce8076625c4b0194011bf7739507fa72e1
Hyeonseo_Lee <lhss1054@naver.com>
2023-08-16 02:00:33 +0900

    feat: add root certificate and make own broker and make data to json format

--- sensor/sensor/sensor.ino
+++ sensor/sensor/sensor.ino
@@ -1,5 +1,6 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
 #include <DHT.h>
 
@@ -7,19 +8,54 @@
 #define dhtType DHT22
 
 DHT dht(dhtPin, dhtType);
-WiFiClient wifiClient;
+WiFiClientSecure wifiClient;
 
 const char* ssid = "AndroidHotspot06_A2_F1";
 const char* password = "****";
-const char* mqttServer = "broker.mqtt-dashboard.com";
-const int mqttPort = 1883;
-const int mqttWsPport = 9001;
+const char* mqttServer = "9c500c1053df40c795c005da44aee8f0.s2.eu.hivemq.cloud";
+const int mqttPort = 8883;
 const char* mqttUsername = "HyeonseoLee";
 const char* mqttPassword = "****";
 
+
+//root certificate
+static const char *root_ca PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)EOF";
+
 PubSubClient mqttClient(wifiClient);
 
-int photoresistor = A1;
+int photoResistor = A1;
 
 void setup(void) {
   Serial.begin(115200);
@@ -27,19 +63,23 @@ void setup(void) {
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  Serial.println("");
+  
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print("-> ");
+    Serial.print(".");
   }
-
+  
   Serial.println("");
   Serial.print("Connected to ");
   Serial.println(ssid);
-  Serial.print("IP addr: ");
+  Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
-  
+
   mqttClient.setServer(mqttServer, mqttPort);
+  wifiClient.setInsecure();
+  wifiClient.setCACert(root_ca);
 
   Serial.println("MQTT client initializing!");
 }
@@ -59,34 +99,31 @@ void loop(void) {
     mqttClient.loop();
 
     //read sensor using dht22
-    int illuminance = analogRead(photoresistor);
+    int illuminance = analogRead(photoResistor);
     float humidityValue = dht.readHumidity();
     float degreeCelsius = dht.readTemperature();
     float degreeFahrenheit = dht.readTemperature(true);
 
-    // StaticJsonDocument<200> jsonDocument;
-    // jsonDocument["illuminance"] = illuminance;
-    // jsonDocument["humidity"] = humidityValue;
-    // jsonDocument["temperatureCelsius"] = degreeCelsius;
-    // jsonDocument["temperatureFahrenheit"] = degreeFahrenheit;
-    // String jsonResponse;
-    // serializeJson(jsonDocument, jsonResponse);
+    //make data to json format
+    StaticJsonDocument<200> jsonDocument;
+    jsonDocument["illuminance"] = illuminance;
+    jsonDocument["humidity"] = humidityValue;
+    jsonDocument["temperatureCelsius"] = degreeCelsius;
+    jsonDocument["temperatureFahrenheit"] = degreeFahrenheit;
+    String jsonResponse;
+    serializeJson(jsonDocument, jsonResponse);
 
     //send data to mqtt broker
-    mqttClient.publish("smartfarm/illuminance", String(illuminance).c_str());
-    mqttClient.publish("smartfarm/humidity", String(humidityValue).c_str());
-    mqttClient.publish("smartfarm/temperatureCelsius", String(degreeCelsius).c_str());
-    mqttClient.publish("smartfarm/temperatureFahrenheit", String(degreeFahrenheit).c_str());
+    mqttClient.publish("smartfarm/sensors", jsonResponse.c_str());
 
-
-    Serial.print("illuminance: ");
+    Serial.print("Illuminance: ");
     Serial.println(illuminance);
-    Serial.print("humidity ");
+    Serial.print("Humidity: ");
     Serial.println(humidityValue);
-    Serial.print("degreeCelsius: ");
+    Serial.print("Temperature Celsius: ");
     Serial.print(degreeCelsius);
     Serial.print("C (");
-    Serial.print("degreeFahrenheit: ");
+    Serial.print("Temperature Fahrenheit: ");
     Serial.print(degreeFahrenheit);
     Serial.println("F)");
 
